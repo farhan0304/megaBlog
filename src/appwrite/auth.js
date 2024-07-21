caca7f9e0859ab03829910e03f0524e81cb812ca
farhan0304 <farhannizam23@gmail.com>
2024-07-21 17:24:39 +0530

    Creating authentication service

---
+++ src/appwrite/auth.js
@@ -0,0 +1,55 @@
+import conf from '../conf/conf'
+import {Client, Account, ID} from 'appwrite'
+
+export class AuthService{
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl) 
+        .setProject(conf.appwriteProjectId);
+
+        this.account = new Account(this.client)
+    }
+    async createAccount({email,password,name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(),email,password,name)
+            if (userAccount) {
+                return await userLogin(email,password)
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw (error)
+        }
+    }
+    async userLogin({email,password}){
+        try {
+            return await this.account.createEmailPasswordSession(email,password)
+        } catch (error) {
+            throw (error)
+        }
+    }
+    async getUser(){
+        try {
+            return this.account.get();
+        } catch (error) {
+            console.log("Authenticate service :: getUser :: Error ", error)
+        }
+        return null;
+    }
+    async userLogout(){
+        try {
+            this.account.deleteSessions();
+        } catch (error) {
+            console.log("Authenticate service :: userLogout :: Error ", error)
+        }
+    }
+
+
+}
+
+const authService = new AuthService();
+
+export default authService;
